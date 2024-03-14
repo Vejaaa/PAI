a49077bf2d00252dfdebfdc436bb9aeff96299a1
Veja Shetty <vejashetty@gmail.com>
2024-03-14 23:01:06 +0530

    Add files via upload

---
+++ Nimgame.py
@@ -0,0 +1,24 @@
+# 2a
+print("Nim game!!\nWe are having 12 tokens")
+def getTokens(curTokens):
+    global tokens
+    print("How many tokens would you like to take? ", end='')
+    take = int(input())
+    if (take < 1 or take > 3):
+        print("Number must be between 1 and 3.\n")
+        getTokens(curTokens)
+        return
+    tokens = curTokens - take
+    print('You take',take ,'tokens.')
+    print(tokens ,'tokens remaining.\n')
+def compTurn(curTokens):
+    global tokens
+    take = curTokens % 4
+    tokens = curTokens - take
+    print ('Computer takes ',take, ' tokens.')
+    print (tokens,' tokens remaining.\n') 
+tokens = 12
+while (tokens > 0):
+    getTokens(tokens)
+    compTurn(tokens)
+print("Computer wins!")
